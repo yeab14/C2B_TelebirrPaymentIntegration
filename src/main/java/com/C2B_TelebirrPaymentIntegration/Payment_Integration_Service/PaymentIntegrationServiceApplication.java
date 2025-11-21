0aa05b474c891dbab6d4dc74df1ff14751e1b49c
Yeabsira Dereje <Yeab149@gmail.com>
2025-11-21 10:48:03 +0200

    first commit

---
+++ src/main/java/com/C2B_TelebirrPaymentIntegration/Payment_Integration_Service/PaymentIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.C2B_TelebirrPaymentIntegration.Payment_Integration_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/C2B_TelebirrPaymentIntegration/Payment_Integration_Service/PaymentIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C2B_TelebirrPaymentIntegration.Payment_Integration_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
